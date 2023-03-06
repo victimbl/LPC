f24f505769d786a0eb3b765bac298a9f8630e9e0
victimbl12 <120356913+victimbl12@users.noreply.github.com>
2023-03-06 13:15:41 -0400

    Add files via upload

---
+++ LPC - pygame/SlimeClash/tile.py
@@ -0,0 +1,8 @@
+from settings import *
+
+
+class Tile(pygame.sprite.Sprite):
+	def __init__(self, pos, groups, image):
+		super().__init__(groups)
+		self.image = image
+		self.rect = self.image.get_rect(topleft=pos)
